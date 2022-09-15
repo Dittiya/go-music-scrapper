c45d6e000a977fe210e8b660350cd8681117d8b1
Aditiya <aditiyaart23@gmail.com>
2022-09-15 17:55:44 +0700

    added router for api and v1

--- router/router.go
+++ router/router.go
@@ -1,5 +1,33 @@
 package router
 
-func BuildEndpoint() {
+import "github.com/gofiber/fiber/v2"
 
+type API struct {
+	fiber.Router
+}
+
+type V1 struct {
+	fiber.Router
+}
+
+func BuildApiEndpoint(app *fiber.App) *API {
+	api := app.Group("/api", func(c *fiber.Ctx) error {
+		msg := "Using api group"
+		c.Set("API-Notes", msg)
+
+		return c.Next()
+	})
+
+	return &API{Router: api}
+}
+
+func BuildV1Endpoint(api *API) *V1 {
+	v1 := api.Group("/v1", func(c *fiber.Ctx) error {
+		msg := "V1"
+		c.Set("Version", msg)
+
+		return c.Next()
+	})
+
+	return &V1{Router: v1}
 }
